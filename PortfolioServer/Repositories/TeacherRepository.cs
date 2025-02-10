d754b2eb6e89c80353f7d61f5e370803941c65c2
fefemf10 <dima.aldakimov@yandex.ru>
2025-02-10 05:31:05 +0300

    attempt to add repositories

---
+++ PortfolioServer/Repositories/TeacherRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioShared.Models;
+using PortfolioShared.ViewModels.Request;
+using System;
+
+namespace PortfolioServer.Repositories
+{
+    public class TeacherRepository
+    {
+        private readonly ApplicationContext db;
+        public TeacherRepository(ApplicationContext db)
+        {
+            this.db = db;
+        }
+        public Task<List<Teacher>> Get()
+        {
+            return db.Teachers.AsNoTracking().ToListAsync();
+        }
+        public Task<List<Teacher>> GetWithDependencies()
+        {
+            return db.Teachers.AsNoTracking().Include(x => x.Faculty).Include(x => x.Department).Include(x => x.Publications).ToListAsync();
+        }
+        public Task<Teacher?> GetById(Guid guid)
+        {
+            return db.Teachers.FindAsync(guid).AsTask();
+        }
+        public Task<Guid> Create(RequestTeacher teacher)
+        {
+            
+        }
+    }
+}
